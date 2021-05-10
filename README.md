288d05d16c3b54498bb4181cf8f462cb92d3b3c2
TSUCHIYA Masatoshi <tsuchm@gmail.com>
2021-05-10 12:55:34 +0900

    Update.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## NICT BERT モデル
 
-[NICT によって配布されている BERTモデル](https://alaginrc.nict.go.jp/nict-bert/index.html)が，正常に動作しているかを確認するため，
+[NICT によって配布されている BERT モデル](https://alaginrc.nict.go.jp/nict-bert/index.html)が，正常に動作しているかを確認するため，
 [公式の実験手順](https://alaginrc.nict.go.jp/nict-bert/Experiments_on_RCQA.html)を，最近の transformers-4.x および datasets で動作するように移植した．移植による変更箇所は以下の2点．
 
  * RCQA を SQuAD 互換に変換するコードを，datasets 用に変更．
@@ -43,9 +43,44 @@ qsub -q full-gpu -l select=1:ncpus=16:ngpus=8 run.sh
   total = 6178
 ```
 
+## 自家製 BERT モデル
+
+[自家製 BERT モデル](https://github.com/tutcsis/bert-japanese)が，正常に動作しているかを確認する手順．
+このリポジトリと同一階層のディレクトリに，[自家製 BERT モデル](https://github.com/tutcsis/bert-japanese)を checkout して学習が完了しているという想定．
+
+```sh
+sh run_jawiki.sh
+```
+
+または，以下のようにバッチジョブとして投入する．
+
+```sh
+qsub -q full-gpu -l select=1:ncpus=16:ngpus=8 run_jawiki.sh
+```
+
+ハイパーパラメータの探索は行っていないが，東北大 BERT モデルとほぼ同等の性能が出るはずである．
+
+```
+***** eval metrics *****
+  HasAns_exact      = 68.4638
+  HasAns_f1         = 71.9656
+  HasAns_total      =    3079
+  NoAns_exact       = 84.1239
+  NoAns_f1          = 84.1239
+  NoAns_total       =    3099
+  best_exact        = 76.3192
+  best_exact_thresh =     0.0
+  best_f1           = 78.0644
+  best_f1_thresh    =     0.0
+  eval_samples      =    6183
+  exact             = 76.3192
+  f1                = 78.0644
+  total             =    6178
+```
+
 ## 東北大 BERT モデル
 
-[東北大によって配布されている BERTモデル](https://huggingface.co/cl-tohoku/bert-base-japanese-v2)が，正常に動作しているかを確認するための手順．実行手順は以下の通り．
+[東北大によって配布されている BERT モデル](https://huggingface.co/cl-tohoku/bert-base-japanese-v2)が，正常に動作しているかを確認するための手順．実行手順は以下の通り．
 
 ```sh
 sh run_tohoku.sh
---
+++ run_jawiki.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+#PBS -q full-gpu
+#PBS -l select=1:ncpus=16:ngpus=8
+
+RCQAFILE="all-v1.0.json.gz"
+RCQAURL="http://www.cl.ecei.tohoku.ac.jp/rcqa/data/${RCQAFILE}"
+
+SCRIPTS="run_qa.py trainer_qa.py utils_qa.py"
+BASEURL="https://raw.githubusercontent.com/huggingface/transformers/v4.4.2-release/examples/question-answering"
+
+DATADIR=data/unidic
+TRAINFILE=${DATADIR}/rcqa_train.json
+VALIDFILE=${DATADIR}/rcqa_dev.json
+TESTFILE=${DATADIR}/rcqa_test.json
+
+MODELDIR=../bert-japanese/models/jawiki-20210301
+MODELNAME=`basename ${MODELDIR}`
+BATCH=32
+LR=5e-5
+EPOCH=2
+
+if [ -n "${PBS_O_WORKDIR}" ]; then
+	if [ "${PBS_ENVIRONMENT}" != PBS_INTERACTIVE ]; then
+		cd "${PBS_O_WORKDIR}"
+	fi
+fi
+
+if [ ! -f ${RCQAFILE} ]; then
+	wget ${RCQAURL}
+fi
+
+if [ ! -f ${TRAINFILE} ]; then
+	mkdir -p ${DATADIR}
+	cd ${DATADIR} && python3 ../../conv.py --rcqafile ../../${RCQAFILE} --unidic
+fi
+
+for f in ${SCRIPTS}; do
+	if [ ! -f ${f} ]; then
+		wget ${BASEURL}/${f}
+	fi
+done
+
+python3 run_qa.py \
+    --model_name_or_path          ${MODELDIR} \
+    --output_dir                  train_output_${MODELNAME}_batch${BATCH}_lr${LR}_epochs${EPOCH} \
+    --train_file                  ${TRAINFILE} \
+    --validation_file             ${VALIDFILE} \
+    --overwrite_cache             \
+    --version_2_with_negative     \
+    --do_train                    \
+    --do_eval                     \
+    --gradient_accumulation_steps 1 \
+    --per_device_train_batch_size $((BATCH / 8)) \
+    --per_device_eval_batch_size  32 \
+    --learning_rate               ${LR} \
+    --num_train_epochs            ${EPOCH} \
+    --save_steps                  10000 \
+    --fp16                        \
+    --fp16_opt_level              O2
+
+python3 run_qa.py \
+    --model_name_or_path          train_output_${MODELNAME}_batch${BATCH}_lr${LR}_epochs${EPOCH} \
+    --output_dir                  eval_output_${MODELNAME}_batch${BATCH}_lr${LR}_epochs${EPOCH} \
+    --train_file                  ${TRAINFILE} \
+    --validation_file             ${TESTFILE} \
+    --overwrite_cache             \
+    --version_2_with_negative     \
+    --do_eval                     \
+    --per_device_eval_batch_size  32
